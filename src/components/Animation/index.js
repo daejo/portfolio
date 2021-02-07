574004c30ec1d5ae4b09e0985f44f1d4cf70ba81
daejo <daejo@icloud.com>
2021-02-06 22:08:20 -0800

    fixes errors for animation

--- src/components/Animation/index.js
+++ src/components/Animation/index.js
@@ -1,8 +1,10 @@
 import styled, { keyframes } from 'styled-components';
 import { fadeInDown } from 'react-animations';
 
+//-- FADE IN DOWN ANIMATION START --//
 const fadeInDownAnimation = keyframes`${fadeInDown}`;
 
 export const FadeInDownDiv = styled.div`
   animation: 1s ${fadeInDownAnimation};
 `;
+//-- FADE IN DOWN ANIMATION END --//
